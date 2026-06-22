b96056d620533f12e14a8cef6affc8187aa84425
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-06-22 15:32:18 +0530

    push

--- coffee-backend/routes/coffeeRoutes.js
+++ coffee-backend/routes/coffeeRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../middleware/upload.js");
 
 const {
   createCoffee,
@@ -9,10 +10,27 @@ const {
   deleteCoffee,
 } = require("../controllers/coffeeController");
 
-router.post("/", createCoffee);
+// Create Coffee with Image
+router.post(
+  "/",
+  upload.single("image"),
+  createCoffee
+);
+
+// Get All Coffee
 router.get("/", getAllCoffee);
+
+// Get Single Coffee
 router.get("/:id", getCoffeeById);
-router.put("/:id", updateCoffee);
+
+// Update Coffee with Image
+router.put(
+  "/:id",
+  upload.single("image"),
+  updateCoffee
+);
+
+// Delete Coffee
 router.delete("/:id", deleteCoffee);
 
 module.exports = router;
