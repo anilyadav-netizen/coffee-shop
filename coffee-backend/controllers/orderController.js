b07d285687e3f63981f04cc984ef1e8c209c494a
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-06-30 11:43:22 +0530

    by  malik

---
+++ coffee-backend/controllers/orderController.js
@@ -0,0 +1,36 @@
+const Order = require("../models/orderModel");
+
+// ===============================
+// Admin - Get All Orders
+// ===============================
+exports.getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({})
+      .populate({
+        path: "user",
+        select: "name email mobile profileImage",
+      })
+      .populate({
+        path: "payment",
+      })
+      .populate({
+        path: "products.coffee",
+        select: "name image price category",
+      })
+      .sort({ createdAt: -1 });
+
+    return res.status(200).json({
+      success: true,
+      totalOrders: orders.length,
+      orders,
+    });
+  } catch (error) {
+    console.error("Get All Orders Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Failed to fetch orders",
+      error: error.message,
+    });
+  }
+};
--- coffee-backend/routes/orderRoutes.js
+++ coffee-backend/routes/orderRoutes.js
@@ -5,9 +5,13 @@ const {
   updateOrderStatus,
 } = require("../controllers/riderController");
 const {protect} = require("../middleware/authMiddleware");
+const { getAllOrders } = require("../controllers/orderController");
 
 
 // Update Order Status
 router.put("/update-status/:orderId", protect, updateOrderStatus);
+
+router.get("/admin/all", protect,  getAllOrders);
+
 
 module.exports = router;
